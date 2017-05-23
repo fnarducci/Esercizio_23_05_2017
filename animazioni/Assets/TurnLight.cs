0b16ee41831ccd9698815f29b9a8a0c6a29cfa71
Luigi Durso <alduin.93y@gmail.com>
2017-05-23 18:02:04 +0200

    Inizio IU
    -UI creata
    -Implementata accensione/spegnimento luce.

---
+++ animazioni/Assets/TurnLight.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TurnLight : MonoBehaviour 
+{
+	private bool accensione;
+	public GameObject pulsante;
+
+	// Use this for initialization
+	void Start () 
+	{
+		this.accensione = true;
+	}
+	
+	// Update is called once per frame
+	void Update () 
+	{
+		
+	}
+
+	public void turnLight()
+	{
+		if (accensione) 
+		{
+			GetComponent<Light> ().enabled = false;
+			pulsante.GetComponent<changeImage> ().changeTexture (false);
+			accensione = false;
+		} else 
+		{
+			GetComponent<Light> ().enabled = true;
+			pulsante.GetComponent<changeImage> ().changeTexture (true);
+			accensione = true;
+		}
+	}
+}
---
+++ animazioni/Assets/changeImage.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class changeImage : MonoBehaviour 
+{
+
+	public Sprite acceso;
+	public Sprite spento;
+
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+	public void changeTexture(bool accensione)
+	{
+		if (accensione) {
+			Image im = GetComponent<Image> ();
+			im.sprite = acceso;
+		} else {
+			Image im = GetComponent<Image> ();
+			im.sprite = spento;
+		}
+	}
+}
